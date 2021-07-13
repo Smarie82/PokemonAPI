927b8edbfe5fa99c0261c3a1077097012e27e7d8
Stephanie Falls <stephms@gmail.com>
2021-07-13 12:18:35 -0400

    seven

--- poke.js
+++ poke.js
@@ -1,9 +1,26 @@
-const baseURL = "https://pokeapi.co/api/v2/type/5/";
+const groundURL = "https://pokeapi.co/api/v2/type/5/";
+const ghostURL = "https://pokeapi.co/api/v2/type/8/";
+const normalURL = "https://pokeapi.co/api/v2/type/1/";
+const fightingURL = "https://pokeapi.co/api/v2/type/2/";
+const flyingURL = "https://pokeapi.co/api/v2/type/3/";
+const poisonURL = "https://pokeapi.co/api/v2/type/4/";
+const rockURL = "https://pokeapi.co/api/v2/type/6/";
+const bugURL = "https://pokeapi.co/api/v2/type/7/";
+const steelURL = "https://pokeapi.co/api/v2/type/9/";
+const fireURL = "https://pokeapi.co/api/v2/type/10/";
+const waterURL = "https://pokeapi.co/api/v2/type/11/";
+const grassURL = "https://pokeapi.co/api/v2/type/12/";
+const electricURL = "https://pokeapi.co/api/v2/type/13/";
+const psychicURL = "https://pokeapi.co/api/v2/type/14/";
+const iceURL = "https://pokeapi.co/api/v2/type/15/";
+const dragonURL = "https://pokeapi.co/api/v2/type/16/";
+const darkURL = "https://pokeapi.co/api/v2/type/17/";
+const fairyURL = "https://pokeapi.co/api/v2/type/18/";
+const unknownURL = "https://pokeapi.co/api/v2/type/10001/";
+const shadowURL = "https://pokeapi.co/api/v2/type/10002/";
 
-// const searchTerm = document.querySelector('search');
-// const searchForm = document.querySelector('form');
-// const submitBtn = document.querySelector('.submit');
 
+//main information for battle stat card
 const battleCard = document.querySelector('section');
 const card = document.querySelector('.card');
 const cardBody = document.querySelector('.card-body');
@@ -13,42 +30,21 @@ const listGroupThree = document.querySelector('.list-group-item-three');
 const listGroupFour = document.querySelector('.list-group-item-four');
 const listGroupFive = document.querySelector('.list-group-item-five');
 
-let pokeTypes = {
-    normal :"https://pokeapi.co/api/v2/type/1/",
-    fighting :"https://pokeapi.co/api/v2/type/2/",
-    flying :"https://pokeapi.co/api/v2/type/3/",
-    poison :"https://pokeapi.co/api/v2/type/4/",
-    ground :"https://pokeapi.co/api/v2/type/5/",
-    rock :"https://pokeapi.co/api/v2/type/6/",
-    bug :"https://pokeapi.co/api/v2/type/7/",
-    ghost :"https://pokeapi.co/api/v2/type/8/",
-    steel :"https://pokeapi.co/api/v2/type/9/",
-    fire :"https://pokeapi.co/api/v2/type/10/",
-    water :"https://pokeapi.co/api/v2/type/11/",
-    grass :"https://pokeapi.co/api/v2/type/12/",
-    electric :"https://pokeapi.co/api/v2/type/13/",
-    psychic :"https://pokeapi.co/api/v2/type/14/",
-    ice :"https://pokeapi.co/api/v2/type/15/",
-    dragon :"https://pokeapi.co/api/v2/type/16/",
-    dark :"https://pokeapi.co/api/v2/type/17/",
-    fairy :"https://pokeapi.co/api/v2/type/18/",
-    unknown :"https://pokeapi.co/api/v2/type/10001/",
-    shadow :"https://pokeapi.co/api/v2/type/10002/",
-}
 
-//searchForm.addEventListener('submit', fetchResults);
-
-fetch(baseURL)
+fetch(groundURL)
     .then(response => response.json())
     .then(json => displayStats(json));
     
-    
+// fetch(ghostURL)
+//     .then(response => response.json())
+//     .then(json => displayStats(json));   
+ 
     function displayStats(battleStats) {
         console.log(battleStats);
 
         let typeName = document.createElement('p');
         typeName.className = 'list-group-item-one';
-        typeName.innerText = battleStats.name;
+        typeName.innerText = battleStats.name.toUpperCase().replaceAll(' ', ' ');
 
         let doubleTo = document.createElement('p');
         doubleTo.className = 'list-group-item-two';
@@ -66,8 +62,6 @@ fetch(baseURL)
         noDamageFrom.className = 'list-group-item-five';
         noDamageFrom.innerText = battleStats.damage_relations.no_damage_from[0].name;
         
-    
-      
 
        listGroupOne.appendChild(typeName);
        listGroupTwo.appendChild(doubleTo);
@@ -75,3 +69,6 @@ fetch(baseURL)
        listGroupFour.appendChild(noDamageTo);
        listGroupFive.appendChild(noDamageFrom);
     }
+
+
+    
